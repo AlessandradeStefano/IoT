ae511f7a3ea0d8aa967964931923d263d9b776d1
alessandra <destefanoalessandra244@gmail.com>
2023-08-19 13:32:07 +0200

    modified messages structure and started to implement connect and subscribe. Commented the idea to implement the publish

--- Topology.h
+++ Topology.h
@@ -3,27 +3,20 @@
 
 #define NUM_TOPICS 3
 #define MAX_SUBS 8
-typedef nx_struct Message{
+typedef nx_struct action_message{
     nx_uint8_t messageType;
     nx_uint16_t sender_ID;
     nx_uint16_t destination;
-    nx_uint8_t status; //???????
+    nx_uint8_t topic;
     nx_uit8_t payload;
-} Message;
+} action_message;
 
 //topics
 #define TEMPERATURE 1
 #define HUMIDITY 2
 #define LUMINOSITY 3
 
-// Subscription information
-typedef nx_struct Subscription {
-        nx_uint8_t topic;
-        nx_uint8_t subscriberID;
-        nx_uint8_t receiver //serve?
-}Subscription;
 
-Subscription subscriptions[NUM_TOPICS][MAX_SUBS];
 // Message types
 enum {
     CONNECT,
